04b864dae3ffa9bb8a9289da5ed37a789ce90c63
lijinbang <sign_lee@163.com>
2022-02-19 15:48:25 +0800

    代码格式整理

--- js/index.js
+++ js/index.js
@@ -1,8 +1,5 @@
-
-  
+// 1.初次渲染  2.dom-diff
 function render(vnode, container) {
-  // 1.初次渲染  2.dom-diff
-  
   patch(null, vnode, container);
 }
 
@@ -13,35 +10,33 @@ function render(vnode, container) {
  * @param {*} container 容器
  */
 
+// 视图渲染
 function patch(n1, n2, container) {
-  // 组件的虚拟节点，tag是一个对象
+  // 如果是n2的tag是string的话，则标明是个标签
   // 如果是组件的话 tag 可能是一个对象
   if (typeof n2.tag === "string") {
     // 标签
-    console.log(container)
     mountElement(n2, container);
-    
   } else if (typeof n2.tag === "object") {
     // 组件渲染
   }
 }
 // 挂载元素
 function mountElement(vnode, container) {
-    const {tag,children,props} = vnode
-    // 将虚拟节点和真实节点创造映射关系
-    let el = (vnode.el = nodeOps.createElement(tag))
-    if(Array.isArray(children)){
-        mountChildren(children,container)
-    }else{
-        nodeOps.hostSetElementText(el,children)
-    }
-    
-    nodeOps.insert(el,container)
+  const { tag, children, props } = vnode;
+  // 将虚拟节点和真实节点创造映射关系
+  let el = (vnode.el = nodeOps.createElement(tag));
+  if (Array.isArray(children)) {
+    mountChildren(children, container);
+  } else {
+    nodeOps.hostSetElementText(el, children);
+  }
+  nodeOps.insert(el, container);
 }
 
-function  mountChildren(children,container){
-    for(let i=0;i<children.length;i++){
-        let child = children[i]
-        patch(null,child,container)//递归挂载子节点
-    }
+function mountChildren(children, container) {
+  for (let i = 0; i < children.length; i++) {
+    let child = children[i];
+    patch(null, child, container); //递归挂载子节点
+  }
 }
