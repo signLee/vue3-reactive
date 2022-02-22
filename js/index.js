84aa7ba660646b2771f0b00e01650cf06b3b7268
sign_lee <sign_lee@163.com>
2022-02-22 17:47:05 +0800

    子节点比对添加，最长递增子序列算法添加

--- js/index.js
+++ js/index.js
@@ -12,21 +12,21 @@ function render(vnode, container) {
  */
 
 // 视图渲染
-function patch(n1, n2, container) {
+function patch(n1, n2, container, anchor) {
   // 如果是n2的tag是string的话，则标明是个标签
   // 如果是组件的话 tag 可能是一个对象
   if (typeof n2.tag === "string") {
     // 标签
-    processElement(n1, n2, container);
+    processElement(n1, n2, container, anchor);
   } else if (typeof n2.tag === "object") {
     // 组件渲染
-    mountComponent(n2, container);
+    mountComponent(n2, containe);
   }
 }
 //元素比对
-function processElement(n1, n2, container) {
+function processElement(n1, n2, container, anchor) {
   if (n1 == null) {
-    mountElement(n2, container) // 初次渲染
+    mountElement(n2, container, anchor) // 初次渲染
   } else {
     patchElement(n1, n2, container)// diff操作
   }
@@ -36,6 +36,114 @@ function patchElement(n1, n2, container) {
   const oldProps = n1.props;
   const newProps = n2.props;
   patchProps(el, oldProps, newProps);
+  patchChildren(n1, n2, el)
+}
+function patchChildren(n1, n2, container) {
+  const c1 = n1.children
+  const c2 = n2.children
+  if (typeof c2 == 'string') {
+    if (c1 !== c2) {
+      nodeOps.hostSetElementText(container, c2)
+    }
+  } else {
+    // 新的子节点为数组
+    if (typeof c1 == 'string') {
+      nodeOps.hostSetElementText(container, '')
+      mountChildren(c2, container)
+    } else {
+      patchKeyedChildren(c1, c2, container);
+    }
+  }
+}
+
+function patchKeyedChildren(c1, c2, container) {
+  let e1 = c1.length - 1
+  let e2 = c2.length - 1
+  // 内部优化处理  头头比较  尾尾比较  头尾比较  尾头比较
+  const keyedToNewIndexMap = new Map()
+  // 1.根据新节点生成一个key->index的映射表
+  for (let i = 0; i <= e2; i++) {
+    const currentEle = c2[i]
+    keyedToNewIndexMap.set(currentEle.props.key, i)
+  }
+  // 2.去老的里边找 看看有没有对应的  如果有一样的就复用
+  const newIndexToOldIndexMap = new Array(e2 + 1)
+  for (let i = 0; i < e2; i++) {
+    newIndexToOldIndexMap[i] = -1
+  }
+  for (let i = 0; i <= e1; i++) {
+    const oldVnode = c1[i]
+    let newIndex = keyedToNewIndexMap.get(oldVnode.props.key)
+    if (newIndex == undefined) {
+      nodeOps.remove(oldVnode.el)
+    } else {
+      // 复用并且比对属性
+      newIndexToOldIndexMap[newIndex] = i + 1
+      patch(oldVnode, c2[newIndex], container)
+    }
+  }
+  let sequence = getSequence(newIndexToOldIndexMap)
+  let j = sequence.length - 1
+  // 以上方法仅仅是比对和删除无用节点，没有移动操作
+  for (let i = e2; i >= 0; i--) {
+    let currentEle = c2[i]
+    const anchor = i + 1 <= e2 ? c2[i + 1].el : null;
+    // 新的比老的多
+    if (newIndexToOldIndexMap[i] === -1) {
+      // 新增元素，插入到列表中
+      patch(null, currentEle, container, anchor)
+    } else {
+      if (i == sequence[j]) {
+        j--
+      } else {
+        // 根据最长递增子序列，来确定不需要移动的元素直接跳过
+        nodeOps.insert(currentEle.el, container, anchor)
+      }
+    }
+  }
+  // 3.新的比老的多做添加处理，老的比新的多就删除
+  // 4.两个key一样的情况 比较属性，移动
+}
+
+function getSequence(arr) {
+  const result = [0]
+  let p = arr.slice()
+  let len = arr.length
+  let i, j, u, v, c;
+  for (i = 0; i < len; i++) {
+    const arrI = arr[i]
+    if (arrI !== 0) {
+      j = result[result.length - 1]
+      if (arr[j] < arrI) {
+        p[i] = j
+        result.push(i)
+        continue
+      }
+      u = 0;
+      v = result.length - 1;
+      while (u < v) {
+        c = ((u + v) / 2) | 0
+        if (arr[result[c]] < arrI) {
+          u = c + 1
+        } else {
+          v = c
+        }
+      }
+      if (arrI < arr[result[u]]) {
+        if (u > 0) {
+          p[i] = result[u - 1]
+        }
+        result[u] = i
+      }
+    }
+  }
+  u = result.length;
+  v = result[u - 1];
+  while (u-- > 0) {
+    result[u] = v;
+    v = p[v]
+  }
+  return result
 }
 function patchProps(el, oldProps, newProps) {
   if (oldProps !== newProps) {
@@ -56,7 +164,7 @@ function patchProps(el, oldProps, newProps) {
   }
 }
 // 挂载元素
-function mountElement(vnode, container) {
+function mountElement(vnode, container, anchor) {
   const { tag, children, props } = vnode;
   // 将虚拟节点和真实节点创造映射关系
   let el = (vnode.el = nodeOps.createElement(tag));
@@ -70,7 +178,7 @@ function mountElement(vnode, container) {
   } else {
     nodeOps.hostSetElementText(el, children);
   }
-  nodeOps.insert(el, container);
+  nodeOps.insert(el, container, anchor);
 }
 // 组件挂载
 function mountComponent(vnode, container) {
